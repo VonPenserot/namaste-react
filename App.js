6f01c4d02240342f3602c1dc2d448c48999cb616
VonPenserot <joachim.penserot@proton.me>
2024-08-07 20:37:55 -0300

    fix: broken import

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "heading"
-);
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "heading1"
-);
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading1]
-);
+function App() {
+  return <div>Hello World!</div>;
+}
 
 // create root using createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing react element inside root
-root.render(container);
+root.render(<App />);
