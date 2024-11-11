75ef28b37cfc41ec3b0f8d00c35aa063fb5034ce
VonPenserot <joachim.penserot@proton.me>
2024-11-11 06:59:46 -0300

    chore: destructure data and pass to component

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,20 +1,24 @@
-export function RestaurantCard({
-  restaurantName,
-  restaurantCuisine,
-  restaurantStars,
-  restaurantDeliveryTime,
-}) {
+import { CDN_URL } from "../../constants";
+
+export function RestaurantCard({ resData }) {
+  const { resData } = props;
+
+  const {
+    cloudinaryImageId,
+    name,
+    avgRating,
+    cuisines,
+    costForTwo,
+    deliveryTime,
+  } = resData;
+
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        className="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_250,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/3/6/dab55604-2e16-46ad-8282-445331b01a0e_6494a846-89f4-48f4-bbfd-9550e5067db7_compressed"
-        alt="Merghana Foods"
-      />
-      <h3>{restaurantName}</h3>
-      <h4>{restaurantCuisine}</h4>
-      <h4>{restaurantStars} stars</h4>
-      <h4>{restaurantDeliveryTime} minutes</h4>
+      <img className="res-logo" src={CDN_URL + cloudinaryImageId} alt={name} />
+      <h3>{name}</h3>
+      <h4>{cuisines}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{deliveryTime} minutes</h4>
     </div>
   );
 }
